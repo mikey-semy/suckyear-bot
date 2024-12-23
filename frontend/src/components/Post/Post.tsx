d7f4e78e881be56148203af6f169c3d169103b30
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-12-23 22:35:22 +0300

    Update Post.tsx

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -3,9 +3,13 @@ import { PostTypes } from './Post.types';
 
 const Post: React.FC<PostTypes> = ({post}) => {
     return (
-        <div>
-            <h1>{post}</h1>
-        </div>
+        <>
+            <h1>{post.title}</h1>
+            <p>Автор: {post.author}</p>
+            <p>Рейтинг: {post.rating}</p>
+            <p>Дата создания: {post.created_at}</p>
+            <p>{post.text}</p>
+        </>
     );
 }
 export default Post;
