6a0e7de3bf404489f2a57080e3624f6902c62a8e
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-12-23 22:36:19 +0300

    Update Post.tsx

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -4,7 +4,6 @@ import { PostTypes } from './Post.types';
 const Post: React.FC<PostTypes> = ({post}) => {
     return (
         <>
-            <h1>{post.title}</h1>
             <p>Автор: {post.author}</p>
             <p>Рейтинг: {post.rating}</p>
             <p>Дата создания: {post.created_at}</p>
